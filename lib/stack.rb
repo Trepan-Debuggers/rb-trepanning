418fd3b8853d3c69db912ac1eb41b49555ccf352
rocky <rockyb@rubyforge.org>
2009-07-18 18:38:29 -0400

    Fill out a tad more.

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,10 +1,24 @@
-# Return a count of 
 class Debugger
   module Stack
+    # Return a count of the number of frames we've got
     def count_frames(frame, count_start=0)
       count = -count-start
       while frame do
         count += 1
+        frame = frame.prev
+      end
+    end
+
+    def print_stack_entry(frame)
+      # To be completed...
+    end
+
+    # Print `count' frame entries
+    def print_stack_trace(frame, count=nil)
+      n = count_frames(frame)
+      n = [n, count].min if count
+      n.downto(1) do |i|
+        print_stack_entry(frame)
         frame = frame.prev
       end
     end
