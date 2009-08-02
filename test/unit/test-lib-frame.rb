50dad178b48707f43b4e2edd4348cb0092f3ef06
rocky <rockyb@rubyforge.org>
2009-08-02 08:02:41 -0400

    unit test administration

--- test/unit/test-lib-frame.rb
+++ test/unit/test-lib-frame.rb
@@ -12,20 +12,20 @@ class TestLibFrame < Test::Unit::TestCase
     frame = RubyVM::ThreadFrame.current
     base_count = frame.stack_size
     s = format_stack_entry(frame)
-    pat = /^METHOD .*#test_lib_frame\(\) \["file", ".*test-lib-frame.rb"\] at line \d+/
-    assert s =~ pat 
+    pat = /^METHOD .*#test_lib_frame\(\) in file .*test-lib-frame.rb at line \d+/
+    assert(s =~ pat, "got #{s}, expected pat #{pat}")
     1.times do 
       assert_equal(base_count+2, RubyVM::ThreadFrame.current.stack_size)
       s = format_stack_entry(frame)
-      assert s =~ pat
+      assert(s =~ pat, "got #{s}, expected pat #{pat}")
     end
 
     def foo(count)
       frame = RubyVM::ThreadFrame.current
       assert_equal(count, frame.stack_size)
       s = format_stack_entry(frame)
-      pat = /^METHOD .*#foo\(count\) \["file", ".*test-lib-frame.rb"\] at line \d+/
-      assert s =~ pat
+      pat = /^METHOD .*#foo\(count\) in file .*test-lib-frame.rb at line \d+/
+      assert(s =~ pat, "got #{s}, expected pat #{pat}")
     end
     foo(base_count+1)
   end
