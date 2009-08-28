9ac2687f097c67d534052a7aa1cb6b6c40362137
rocky <rockyb@rubyforge.org>
2009-08-27 22:49:16 -0400

    More small doc updates

--- emacs/rbdbgr-regexp.el
+++ emacs/rbdbgr-regexp.el
@@ -17,8 +17,10 @@ a string output by a debugger"
   (line-group :type integer))
 
 (defvar rbdbg-dbgr-pat-hash (make-hash-table :test 'equal)
-  "Hash indexed by debugger name of rbdbg-dbgr-pat")
+  "Hash key is the debugger (string) name. The value of the hash
+  rbdbg-dbgr-loc-pat struct")
 
+; Create one for the Ruby 1.9 debugger "rbdbgr". 
 (setf (gethash "rbdbgr" rbdbg-dbgr-pat-hash)
       (make-rbdbg-dbgr-loc-pat
        :regexp ".. (\\([-a-zA-Z0-9_/.]*\\):\\([0-9]+\\))"
