4f77d41b657e6a8170b782ad47ab56aa9fcbcd42
rocky <rockyb@rubyforge.org>
2009-08-28 06:42:02 -0400

    Add regexps for more languages

--- emacs/rbdbgr-regexp.el
+++ emacs/rbdbgr-regexp.el
@@ -10,15 +10,18 @@
 ;;
 
 (defstruct rbdbg-dbgr-loc-pat
-  "Information to match and extract a file, line number location from
-a string output by a debugger"
+  "Information to match and extract a file and line number location from
+a string output by a debugger inside a process shell"
   (regexp     :type string)
   (file-group :type integer)
   (line-group :type integer))
 
 (defvar rbdbg-dbgr-pat-hash (make-hash-table :test 'equal)
-  "Hash key is the debugger (string) name. The value of the hash
+  "Hash key is the debugger name, a string. The values of a hash entry
   is a rbdbg-dbgr-loc-pat struct")
+
+;; FIXME? If this file gets too long and cumbersome, split each debugger
+;; section to its own file.
 
 ; Create one for the Ruby 1.9 debugger "rbdbgr". 
 (setf (gethash "rbdbgr" rbdbg-dbgr-pat-hash)
@@ -26,6 +29,27 @@ a string output by a debugger"
        :regexp ".. (\\([-a-zA-Z0-9_/.]*\\):\\([0-9]+\\))"
        :file-group 1
        :line-group 2))
+
+; Create one for the Korn Shell debugger "kshdb". 
+(setf (gethash "kshdb" rbdbg-dbgr-pat-hash)
+      (make-rbdbg-dbgr-loc-pat
+       :regexp "\\(^\\|\n\\)(\\([^:]+\\):\\([0-9]*\\))"
+       :file-group 2
+       :line-group 3))
+
+; Now one for the Python debugger "pydbgr". 
+(setf (gethash "pydbgr" rbdbg-dbgr-pat-hash)
+      (make-rbdbg-dbgr-loc-pat
+       :regexp "^(\\(\\(?:[a-zA-Z]:\\)?[-a-zA-Z0-9_/.\\\\ ]+\\):\\([0-9]+\\))"
+       :file-group 1
+       :line-group 2))
+
+; And one for the Bash debugger "bashdb". 
+(setf (gethash "bashdb" rbdbg-dbgr-pat-hash)
+      (make-rbdbg-dbgr-loc-pat
+       :regexp "\\(^\\|\n\\)(\\([^:]+\\):\\([0-9]*\\))"
+       :file-group 2
+       :line-group 3))
 
 (provide 'rbdbgr-regexp)
 
