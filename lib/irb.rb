2802fdfe7cf1e349ad6353a58188ffc835762189
rocky <rockyb@rubyforge.org>
2009-09-17 06:41:32 -0400

    Add irb command (rudimentary). Yay!

---
+++ lib/irb.rb
@@ -0,0 +1,50 @@
+# This code comes directly from ruby-debug.
+require 'irb'
+module IRB # :nodoc:
+  module ExtendCommand # :nodoc:
+    class Continue # :nodoc:
+      def self.execute(conf)
+        throw :IRB_EXIT, :cont
+      end
+    end
+    class Next # :nodoc:
+      def self.execute(conf)
+        throw :IRB_EXIT, :next
+      end
+    end
+    class Step # :nodoc:
+      def self.execute(conf)
+        throw :IRB_EXIT, :step
+      end
+    end
+  end
+  ExtendCommandBundle.def_extend_command 'cont', :Continue
+  ExtendCommandBundle.def_extend_command 'n', :Next
+  ExtendCommandBundle.def_extend_command 'step', :Step
+  
+  def self.start_session(binding)
+    unless @__initialized
+      args = ARGV.dup
+      ARGV.replace([])
+      IRB.setup(nil)
+      ARGV.replace(args)
+      @__initialized = true
+    end
+    
+    workspace = WorkSpace.new(binding)
+
+    irb = Irb.new(workspace)
+
+    @CONF[:IRB_RC].call(irb.context) if @CONF[:IRB_RC]
+    @CONF[:MAIN_CONTEXT] = irb.context
+
+    catch(:IRB_EXIT) do
+      irb.eval_input
+    end
+  end
+end
+
+if __FILE__ == $0
+  # Demo it.
+  IRB.start_session(binding) if ARGV.size > 0
+end
---
+++ processor/command/irb.rb
@@ -0,0 +1,68 @@
+require 'irb'
+require_relative 'base_cmd'
+require_relative %w(.. .. lib irb)
+class Debugger::Command::IRBCommand < Debugger::Command
+
+  unless defined?(HELP)
+    HELP = 
+"          irb [-d]\tstarts an Interactive Ruby (IRB) session.
+
+If -d is added you can get access to debugger state via the global variable
+$RDEBUG_state. 
+
+irb is extended with methods 'cont', 'n' and 'step' which 
+run the corresponding debugger commands. In contrast to the real debugger
+commands these commands don't allow command arguments.
+"
+
+    CATEGORY     = 'support'
+    MAX_ARGS     = 1  # Need at most this many
+    NAME         = File.basename(__FILE__, '.rb')
+    SHORT_HELP  = 'Run interactive Ruby session irb as a command subshell'
+  end
+    
+  # This method runs the command
+  def run(args) # :nodoc
+    if args.size > 1
+      add_debugging = '-d' == args[0]
+      # FIXME -d ? 
+    end
+
+    # unless @state.interface.kind_of?(LocalInterface)
+    #   print "Command is available only in local mode.\n"
+    #   throw :debug_error
+    # end
+
+    save_trap = trap("SIGINT") do
+      throw :IRB_EXIT, :cont if $rdebug_in_irb
+    end
+
+    $rdebug_frame = @proc.frame if add_debugging
+    $rdebug_in_irb = true
+    cont = IRB.start_session(@proc.frame.binding)
+    # case cont
+    # when :cont
+    #   @state.proceed 
+    # when :step
+    #   force = Command.settings[:force_stepping]
+    #   @state.context.step(1, force)
+    #   @state.proceed 
+    # when :next
+    #   force = Command.settings[:force_stepping]
+    #   @state.context.step_over(1, @state.frame_pos, force)
+    #   @state.proceed 
+    # else
+    #   file = @state.context.frame_file(0)
+    #   line = @state.context.frame_line(0)
+    #   CommandProcessor.print_location_and_text(file, line)
+    #   @state.previous_line = nil
+    # end
+  end
+end
+
+if __FILE__ == $0
+  require_relative %w(.. mock)
+  name = File.basename(__FILE__, '.rb')
+  dbgr, cmd = MockDebugger::setup(name)
+  MockDebugger::show_special_class_constants(cmd)
+end
