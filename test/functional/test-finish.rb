52bb87b6aeeea4e7939f4cc894d60436de75fe03
R. Bernstein <rocky@gnu.org>
2009-09-28 09:42:01 -0400

    Start functional test for "finish".

---
+++ test/functional/test-finish.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+require 'test/unit'
+require 'trace'
+require_relative 'fn_helper'
+
+class TestFinish < Test::Unit::TestCase
+
+  include FnTestHelper
+
+  def test_finish_between_fn
+    
+    # Next over a function
+    def fact(x)
+      return 1 if x <= 1
+      x = x * fact(x-1)
+      return x
+    end
+    cmds = %w(step finish) + ['p x', 'continue'] 
+    d = strarray_setup(cmds)
+    d.start
+    ##############################
+    x = fact(4)
+    y = 5
+    ##############################
+    d.stop # ({:remove => true})
+    out = ['-- x = fact(4)',
+           '-> def fact(x)',
+           '<- end',
+           '24']
+    compare_output(out, d, cmds)
+  end
+  
+end
+
+
+
+
+
+
