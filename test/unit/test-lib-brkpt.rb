8afbc2cf4ea1a3ad6e0da28e14be679cf6856af6
rocky <rockyb@rubyforge.org>
2009-10-18 00:28:46 -0400

    track breakpoint interface change.

--- test/unit/test-lib-brkpt.rb
+++ test/unit/test-lib-brkpt.rb
@@ -11,7 +11,7 @@ class TestLibBrkpt < Test::Unit::TestCase
     b1 = Breakpoint.new(false, 0, iseq)
     assert_equal(false, b1.temp?)
     assert_equal(0, b1.hits)
-    assert_equal(true, b1.condition?(tf))
+    assert_equal(true, b1.condition?(tf.binding))
     assert_equal(1, b1.hits)
     assert_equal(b1.source_container, tf.source_container)
     assert_raises TypeError do 
