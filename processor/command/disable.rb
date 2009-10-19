4bd49c892f3c57866611fb05d6f1fcfc697fce43
R. Bernstein <rocky@gnu.org>
2009-10-19 15:04:00 -0400

    Add "disable" command. More work is needed. Bugs in handling enable/disable.

---
+++ processor/command/disable.rb
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+require_relative %w(base cmd)
+require_relative %w(.. breakpoint)
+require_relative %w(.. .. lib brkpt)
+class Debugger::Command::DisableCommand < Debugger::Command
+
+  unless defined?(HELP)
+    HELP = 
+      'disable [display] bpnumber [bpnumber ...]
+    
+Disables the breakpoints given as a space separated list of breakpoint
+numbers. See also "info break" to get a list.
+'
+
+    CATEGORY      = 'breakpoints'
+    NAME          = File.basename(__FILE__, '.rb')
+    SHORT_HELP    = 'Disable some breakpoints'
+  end
+  
+  def run(args)
+    if args.size == 1
+      errmsg('No breakpoint number given.')
+      return
+    end
+#   if args[1] == 'display'
+#     display_enable(args[2:], 0)
+#   end
+    first = args.shift
+      args.each do |num_str|
+      i = @proc.get_an_int(num_str)
+      success = @proc.en_disable_breakpoint_by_number(num_str.to_i, false) if i
+      msg('Breakpoint %s disabled.' % i) if success
+    end
+  end
+end
+        
+
+if __FILE__ == $0
+  require_relative %w(.. mock)
+  name = File.basename(__FILE__, '.rb')
+  dbgr, cmd = MockDebugger::setup(name)
+  p cmd.run([name])
+end
