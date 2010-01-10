dcbcf71b38af53bda4b55131df8d45f307f6359d
rocky <rockyb@rubyforge.org>
2010-01-10 12:19:30 -0500

    Rename unit test with "lib" to "app" since "app" is where they now live.

--- test/unit/test-app-frame.rb
+++ test/unit/test-app-frame.rb
@@ -7,11 +7,11 @@ class TestLibFrame < Test::Unit::TestCase
 
   include Debugger::Frame
 
-  def test_lib_frame
+  def test_app_frame
     frame = RubyVM::ThreadFrame.current
     base_count = frame.stack_size
     s = format_stack_entry(frame)
-    pat = /^METHOD .*#test_lib_frame\(\) in file .*test-lib-frame.rb at line \d+/
+    pat = /^METHOD .*#test_app_frame\(\) in file .*test-app-frame.rb at line \d+/
     assert(s =~ pat, "got #{s}, expected pat #{pat}")
     1.times do 
       assert_equal(base_count+2, RubyVM::ThreadFrame.current.stack_size)
@@ -23,7 +23,7 @@ class TestLibFrame < Test::Unit::TestCase
       frame = RubyVM::ThreadFrame.current
       assert_equal(count, frame.stack_size)
       s = format_stack_entry(frame)
-      pat = /^METHOD .*#foo\(count\) in file .*test-lib-frame.rb at line \d+/
+      pat = /^METHOD .*#foo\(count\) in file .*test-app-frame.rb at line \d+/
       assert(s =~ pat, "got #{s}, expected pat #{pat}")
     end
     foo(base_count+1)
