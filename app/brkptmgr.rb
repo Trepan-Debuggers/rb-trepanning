f10b7fc4961df4bade049526e26bcbc4373ef5d2
rocky <rockyb@rubyforge.org>
2010-01-25 00:22:00 -0500

    Track unique breakpoint offset locations for purposes of deleting breakpoints. We can have multiple breakpoints at a given iseq offsets.

--- app/brkptmgr.rb
+++ app/brkptmgr.rb
@@ -1,14 +1,18 @@
+require 'set'
 require_relative 'brkpt'
 class BreakpointMgr
 
   attr_reader :list
+  attr_reader :set
 
   def initialize
     @list = []
+    @set = Set.new
   end
 
   def <<(brkpt)
     @list << brkpt
+    @set.add(set_key(brkpt))
   end
 
   def [](index)
@@ -30,15 +34,17 @@ class BreakpointMgr
     end
   end
 
-  def delete_by_brkpt(bp)
-    bp.unset
-    @list = @list.reject{|candidate| candidate == bp}
-    return bp
+  def delete_by_brkpt(delete_bp)
+    @list = @list.reject{|candidate| candidate == delete_bp}
+    @set  = Set.new(@list.map{|bp| set_key(bp)})
+    delete_bp.unset unless @set.member?(set_key(delete_bp))
+    return delete_bp
   end
 
   def add(*args)
     brkpt = Breakpoint.new(*args)
     @list << brkpt
+    @set.add(set_key(brkpt))
     return brkpt
   end
 
@@ -69,6 +75,11 @@ class BreakpointMgr
     end
   end
 
+  # Key used in @set to list unique instruction-sequence offsets.
+  def set_key(bp)
+    [bp.iseq, bp.offset]
+  end
+
   def size
     @list.size
   end
@@ -76,21 +87,46 @@ class BreakpointMgr
   def reset
     @list.each{|bp| bp.unset}
     @list = []
+    @set  = Set.new
   end
 
 end
 if __FILE__ == $0
-  iseq = RubyVM::ThreadFrame.current.iseq
+  def bp_status(brkpts, i)
+    puts "list size: #{brkpts.list.size}"
+    puts "set size: #{brkpts.set.size}"
+    p brkpts
+    puts "--- #{i} ---"
+  end
+
+  frame = RubyVM::ThreadFrame.current 
+  iseq = frame.iseq
   brkpts = BreakpointMgr.new
   brkpts.add(false, 0, iseq)
   p brkpts[2]
-  b2 = Breakpoint.new(false, 0, iseq)
+  bp_status(brkpts, 1)
+  offset = frame.pc_offset
+  b2 = Breakpoint.new(false, offset, iseq)
   brkpts << b2
   p brkpts.find(b2.iseq, b2.offset, nil)
   p brkpts[2]
-  p '---'
+  puts '--- 2 ---'
   p brkpts.line_breaks(iseq.source_container)
-  p '---'
   p brkpts.delete(2)
   p brkpts[2]
+  bp_status(brkpts, 3)
+
+  # Two of the same breakpoints but delete 1 and see that the
+  # other still stays
+  offset = frame.pc_offset
+  b2 = Breakpoint.new(false, offset, iseq)
+  brkpts << b2
+  bp_status(brkpts, 4)
+  b3 = Breakpoint.new(false, offset, iseq)
+  brkpts << b3
+  bp_status(brkpts, 5)
+  brkpts.delete_by_brkpt(b2)
+  bp_status(brkpts, 6)
+  brkpts.delete_by_brkpt(b3)
+  bp_status(brkpts, 7)
 end
--- test/unit/test-app-brkptmgr.rb
+++ test/unit/test-app-brkptmgr.rb
@@ -19,10 +19,36 @@ class TestLibBrkptMgr < Test::Unit::TestCase
     assert_equal(b1, brkpts.find(iseq, offset, tf.binding))
     assert_equal(b1, brkpts.delete(b1.id))
     assert_equal(0, brkpts.size)
+
+    # Try adding via << rather than .add
     b2 = brkpts << Breakpoint.new(true, offsets[1], iseq)
 
     assert_equal(nil, brkpts.find(iseq, offset, tf.binding))
     brkpts.reset
     assert_equal(0, brkpts.size)
   end
+
+  def test_multiple_brkpt_per_offset
+    tf = RubyVM::ThreadFrame.current
+    iseq = tf.iseq
+    offsets = iseq.offsetlines.keys
+    offset  = offsets[0]
+    brkpts = BreakpointMgr.new
+    b1 = brkpts.add(false, offset, iseq)
+    b2 = brkpts.add(false, offset, iseq)
+    assert_equal(2, brkpts.size)
+    assert_equal(1, brkpts.set.size, 
+                 'Two breakpoints but only one iseq/offset')
+    brkpts.delete_by_brkpt(b1)
+    assert_equal(1, brkpts.size, 
+                 'One breakpoint after 2nd breakpoint deleted')
+    assert_equal(1, brkpts.set.size, 
+                 'Two breakpoints, but only one iseq/offset')
+    brkpts.delete_by_brkpt(b2)
+    assert_equal(0, brkpts.size, 
+                 'Both breakpoints deleted')
+    assert_equal(0, brkpts.set.size, 
+                 'Second breakpoint delete should delete iseq/offset')
+  end
+
 end
