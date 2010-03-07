6f2c683de2a4ed4edf7702cb181234f0b5e326cc
R. Bernstein <rocky@gnu.org>
2010-03-06 22:35:33 -0500

    deubg.rb: works better now. Not ideal, but much better.

--- processor/command/debug.rb
+++ processor/command/debug.rb
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+require 'thread_frame'
 require_relative %w(base cmd)
 
 class Debugger::Command::DebugCommand < Debugger::Command
@@ -17,15 +18,14 @@ Enter the debugger recursively on RUBY-CODE."
   end
 
   def run(args)
-    old_tracing = Thread.current.tracing
+    th          = Thread.current
     frame       = @proc.frame  # gets messed up in recursive call
     arg_str     = args[1..-1].join(' ')
-    hidelevels  = @proc.hidelevels[Thread.current]
+    hidelevels  = @proc.hidelevels[th]
 
     # FIXME save/restore hidelevels so "where" doesn't show debugger
     stack_diff = RubyVM::ThreadFrame.current.stack_size - frame.stack_size 
-    p stack_diff
-    @proc.hidelevels[Thread.current] += stack_diff + 1
+    @proc.hidelevels[th] += stack_diff + 1
 
     # Ignore tracing in support routines:
     # this method and debug_eval.
@@ -34,17 +34,28 @@ Enter the debugger recursively on RUBY-CODE."
     eval_me = @proc.method(:debug_eval)
     # FIXME if eval_me is added to ignore
     # we skip over things eval calls.
-    [me].each do |m|
+    [me, eval_me].each do |m|
       tf << m unless tf.member?(m)
     end
 
+    old_tracing            = th.tracing
+    old_exec_event_tracing = th.exec_event_tracing
+    old_step_count         = @proc.core.step_count
+    old_next_level         = @proc.next_level
+
     msg 'ENTERING RECURSIVE DEBUGGER'
-    Thread.current.tracing = false
+    th.tracing             = false
+    th.exec_event_tracing  = false
+    @proc.core.step_count  = 0
+    @proc.next_level       = 32000
     @proc.debug_eval(arg_str)
-    Thread.current.tracing = old_tracing
+    th.exec_event_tracing  = old_exec_event_tracing
+    th.tracing             = old_tracing
     msg 'LEAVING RECURSIVE DEBUGGER'
     @proc.frame_setup(frame)
-    @proc.hidelevels[Thread.current] = hidelevels
+    @proc.hidelevels[th]   = hidelevels
+    @proc.core.step_count  = old_step_count
+    @proc.next_level       = old_next_level
     @proc.print_location
   end
 end
@@ -54,5 +65,7 @@ if __FILE__ == $0
   name = File.basename(__FILE__, '.rb')
   dbgr, cmd = MockDebugger::setup(name)
   name = File.basename(__FILE__, '.rb')
+  cmd.proc.hidelevels[Thread.current] = 0
+  cmd.proc.frame_setup(RubyVM::ThreadFrame::current)
   cmd.run([name, 'x = 1; y = 2'])
 end
