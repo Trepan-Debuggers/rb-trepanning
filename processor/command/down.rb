45dbc9ccea96f5de14154e90372ed524c6012a48
rocky <rockyb@rubyforge.org>
2010-09-24 18:06:17 -0400

    DRY a little by subclassing similar commands.

--- processor/command/down.rb
+++ processor/command/down.rb
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2010 Rocky Bernstein <rockyb@rubyforge.net>
-require_relative 'base/cmd'
+require_relative 'up'
 
-class Trepan::Command::DownCommand < Trepan::Command
+# Debugger "down" command. Is the same as the "up" command with the 
+# direction (set by DIRECTION) reversed.
+class Trepan::Command::DownCommand < Trepan::Command::UpCommand
 
-  unless defined?(HELP)
-    HELP = 
+  # Silence already initialized constant .. warnings
+  old_verbose = $VERBOSE  
+  $VERBOSE    = nil
+  HELP = 
 "d(own) [count]
 
 Move the current frame down in the stack trace (to a newer frame). 0
@@ -14,45 +18,16 @@ is the most recent frame. If no count is given, move down 1.
 See also 'up' and 'frame'.
 "
 
-    ALIASES       = %w(d)
-    CATEGORY      = 'stack'
-    MAX_ARGS      = 1  # Need at most this many
-    NAME          = File.basename(__FILE__, '.rb')
-    NEED_STACK    = true
-    SHORT_HELP    = 'Move frame in the direction of the caller of the last-selected frame'
+  ALIASES       = %w(d)
+  NAME          = File.basename(__FILE__, '.rb')
+  SHORT_HELP    = 'Move frame in the direction of the caller of the last-selected frame'
+  $VERBOSE      = old_verbose 
+
+  def initialize(proc)
+    super
+    @direction = -1 # +1 for up.
   end
-  
-  require_relative '../../app/frame'
-  include Trepan::Frame
 
-  # Run 'down' command. 
-  def run(args)
-
-    # FIXME: move into @proc and test based on NEED_STACK.
-    if not @proc.top_frame
-      errmsg("Program has no stack frame set.")
-      return false
-    end
-
-    if args.size == 1
-      # Form is: "down" which means "down 1"
-      count = 1
-    else
-      stack_size = @proc.top_frame.stack_size - @proc.hide_level
-      count_str = args[1]
-      name_or_id = args[1]
-      opts = {
-        :msg_on_error => 
-        "The 'down' command argument must eval to an integer. Got: %s" % count_str,
-        :min_value => -stack_size,
-        :max_value => stack_size-1
-      }
-      count = @proc.get_an_int(count_str, opts)
-      return false unless count
-    end
-    @proc.adjust_frame(-count, false)
-    return false
-  end
 end
 
 if __FILE__ == $0
--- processor/command/up.rb
+++ processor/command/up.rb
@@ -1,10 +1,15 @@
 # Copyright (C) 2010 Rocky Bernstein <rockyb@rubyforge.net>
 require_relative 'base/cmd'
 
+# up command. Like 'down' butthe direction (set by DIRECTION) is different.
+#
+# NOTE: The down command  subclasses this, so beware when changing! 
 class Trepan::Command::UpCommand < Trepan::Command
 
-  unless defined?(HELP)
-    HELP = 
+  # Silence already initialized constant .. warnings
+  old_verbose = $VERBOSE  
+  $VERBOSE    = nil
+  HELP        = 
 "u(p) [count]
 
 Move the current frame up in the stack trace (to an older frame). 0 is
@@ -13,16 +18,21 @@ the most recent frame. If no count is given, move up 1.
 See also 'down' and 'frame'.
 "
 
-    ALIASES       = %w(u)
-    CATEGORY      = 'stack'
-    MAX_ARGS      = 1  # Need at most this many
-    NAME          = File.basename(__FILE__, '.rb')
-    NEED_STACK    = true
-    SHORT_HELP    = 'Move frame in the direction of the caller of the last-selected frame'
-  end
+  ALIASES       = %w(u)
+  CATEGORY      = 'stack'
+  MAX_ARGS      = 1  # Need at most this many
+  NAME          = File.basename(__FILE__, '.rb')
+  NEED_STACK    = true
+  SHORT_HELP    = 'Move frame in the direction of the caller of the last-selected frame'
+  $VERBOSE      = old_verbose 
   
   require_relative '../../app/frame'
   include Trepan::Frame
+
+  def initialize(proc)
+    super
+    @direction = +1 # -1 for down.
+  end
 
   # Run 'up' command. 
   def run(args)
@@ -42,14 +52,14 @@ See also 'down' and 'frame'.
       name_or_id = args[1]
       opts = {
         :msg_on_error => 
-        "The 'up' command argument must eval to an integer. Got: %s" % count_str,
+        "The '#{NAME}' command argument must eval to an integer. Got: %s" % count_str,
         :min_value => -stack_size,
         :max_value => stack_size-1
       }
       count = @proc.get_an_int(count_str, opts)
       return false unless count
     end
-    @proc.adjust_frame(count, false)
+    @proc.adjust_frame(@direction * count, false)
   end
 end
 
@@ -62,7 +72,7 @@ if __FILE__ == $0
 
   def sep ; puts '=' * 40 end
   cmd.run [name]
-  %w(0 -1 1).each do |count| 
+  %w(-1 0 1 -2).each do |count| 
     puts "#{name} #{count}"
     cmd.run([name, count])
     sep 
