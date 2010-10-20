731034dab0e5262a177b2bef7b89ec0f8ea3b869
rocky <rockyb@rubyforge.org>
2010-10-20 15:02:01 -0400

    Elision of long strings is now in middle of string rather than the end.

--- test/unit/test-app-util.rb
+++ test/unit/test-app-util.rb
@@ -7,11 +7,10 @@ class TestAppUtil < Test::Unit::TestCase
   def test_safe_repr
     string = 'The time has come to talk of many things.'
     assert_equal(string, safe_repr(string, 50))
-    assert_equal('The time has come...', safe_repr(string, 17))
-    assert_equal('The time has come', safe_repr(string, 17, ''))
-    assert_equal('"The time has co"...', safe_repr(string.inspect, 17))
+    assert_equal('The time...  things.', safe_repr(string, 17))
+    assert_equal('"The tim... things."', safe_repr(string.inspect, 17))
     string = "'The time has come to talk of many things.'"
-    assert_equal("'The time has co'...", safe_repr(string, 17))
+    assert_equal("'The tim... things.'", safe_repr(string, 17))
   end
 
 end
