807dc97569aa44594f348ddabc3c87f38556f2f7
rocky <rockyb@rubyforge.org>
2010-10-27 10:14:58 -0400

    Some small omissions.

--- lib/trepanning.rb
+++ lib/trepanning.rb
@@ -20,7 +20,7 @@ ISEQS__        = {} unless
   defined?(ISEQS__) && ISEQS__.is_a?(Hash)
 
 class Trepan
-  VERSION = '0.0.6.git'
+  VERSION = '0.0.6'
 
   attr_accessor :core         # access to Trepan::Core instance
   attr_accessor :intf         # Array. The way the outside world
