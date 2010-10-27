8635c97ba8161d17e0677970bb36e4ff75a3efdd
rocky <rockyb@rubyforge.org>
2010-10-27 10:04:29 -0400

    Update version number for release.

--- lib/trepanning.rb
+++ lib/trepanning.rb
@@ -20,7 +20,7 @@ ISEQS__        = {} unless
   defined?(ISEQS__) && ISEQS__.is_a?(Hash)
 
 class Trepan
-  VERSION = '0.0.5.git'
+  VERSION = '0.0.5'
 
   attr_accessor :core         # access to Trepan::Core instance
   attr_accessor :intf         # Array. The way the outside world
