8bc52135f487610ef1326d5e6095b44c565d2b37
rocky <rockyb@rubyforge.org>
2010-12-27 17:22:09 -0500

    .gemspec: need 1.9.2frame main.rb: wrong fn any -> all? But will rewrite more completely later. test-app-file.rb: remove debug statement.

--- processor/main.rb
+++ processor/main.rb
@@ -257,7 +257,7 @@ class Trepan
           current_command = @macros[macro_cmd_name].call(*args[1..-1])
           msg current_command if settings[:debugmacro]
           if current_command.is_a?(Array) && 
-              current_command.any {|val| !val.is_a?(String)}
+              current_command.all? {|val| val.is_a?(String)}
             args = current_command
           elsif current_command.is_a?(String)
             args = current_command.split
