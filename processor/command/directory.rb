bab936b07f835aafddc2f84ed90933a780b5e8a5
rocky <rockyb@rubyforge.org>
2011-01-11 18:14:16 -0500

    revise help and glob expand directory. More in (sync with suitable adjustments) to rbx-trepanning

--- processor/command/directory.rb
+++ processor/command/directory.rb
@@ -8,22 +8,30 @@ class Trepan::Command::DirectoryCommand < Trepan::Command
     MAX_ARGS     = 1  # Need at most this many
     NAME         = File.basename(__FILE__, '.rb')
     HELP = <<-HELP
+#{NAME} [DIR]
+
 Add directory DIR to beginning of search path for source files.
 DIR can also be $cwd for the current working directory, or $cdir for the
 directory in which the debugged file start.
 With no argument, reset the search path to $cdir:$cwd, the default.
 
-This command may be useful for debugging into Rubinius methods such as
-kernel/common/module.rb if have the source code somewhere.
+This command may be useful if for some reason the debugger can't find 
+source files because directories have been moved. 
+
+Examples:
+   #{NAME} ~/.rvm/src/ruby-head  # Adds an rvm-like directory to path
+   #{NAME} # reset to $cdir:$cwd
       HELP
+
     SHORT_HELP  = 
       'Add directory DIR to beginning of search path for source files'
   end
     
   # This method runs the command
-  def run(args)
+  def run(args) # :nodoc
     if args.size > 1
-      settings[:directory] = "#{args[1]}:#{settings[:directory]}"
+      path = File.expand_path(args[1])
+      settings[:directory] = "#{path}:#{settings[:directory]}"
       msg "Source directories searched: #{settings[:directory]}"
     else
       if confirm('Reintialize source path to empty?', false)
@@ -36,8 +44,7 @@ end
 
 if __FILE__ == $0
   require_relative '../mock'
-  name = File.basename(__FILE__, '.rb')
-  dbgr, cmd = MockDebugger::setup(name)
-  cmd.run([name])
-  cmd.run([name, '/tmp'])
+  dbgr, cmd = MockDebugger::setup
+  cmd.run([cmd.name])
+  cmd.run([cmd.name, '/tmp'])
 end
