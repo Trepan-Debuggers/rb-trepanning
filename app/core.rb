dc0bc38a113e1f376ab136555848771ccdaa5ab1
rocky <rockyb@rubyforge.org>
2011-02-18 20:22:31 -0500

    Change default event set.

--- app/core.rb
+++ app/core.rb
@@ -55,17 +55,12 @@ class Trepan
         :step_count        => 0,                # Stop at next event
         :async_events      => ASYNC_EVENT_MASK,
 
-        # FIXME:
-        # Somewhere inside the VM we allow I guess nested tracing which is messing
-        # up ThreadFrame pointers and information. When this is fixed we can do the below.
-        # Until then we need to at least remove C calls and returns and possibly other
-        # events as well.
-        # :step_events       =>  
-	# (DEFAULT_EVENT_MASK | INSN_EVENT_MASK)
-
+        # Not sure what the "right" set really is. The below is just
+        # a guess. Use "set events" or customize in ~/.trepanrc
         :step_events       =>  
-	(DEFAULT_EVENT_MASK | INSN_EVENT_MASK) &
-	~(C_CALL_EVENT_MASK | C_RETURN_EVENT_MASK)
+#	(DEFAULT_EVENT_MASK | INSN_EVENT_MASK) &
+	(DEFAULT_EVENT_MASK ) &
+	~(C_CALL_EVENT_MASK | C_RETURN_EVENT_MASK | SEND_EVENT_MASK)
       } 
 
     end
