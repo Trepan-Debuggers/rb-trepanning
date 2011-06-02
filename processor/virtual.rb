5a34fec700ef97bdbc48fe99e56a371328138c7d
rocky <rockyb@rubyforge.org>
2011-06-02 10:25:50 -0400

    Add stripped down version of I/O routines. Sync with rbx-trepanning.

--- processor/virtual.rb
+++ processor/virtual.rb
@@ -16,6 +16,18 @@ class Trepan
   class VirtualCmdProcessor
     attr_reader :settings
     def initialize(interfaces, settings={})
+      @interfaces      = interfaces
+      @intf            = interfaces[-1]
+      @settings        = settings
+    end
+    def errmsg(message)
+      puts "Error: #{message}"
+    end
+    def msg(message)
+      puts message
+    end
+    def section(message, opts={})
+      puts "Section: #{message}"
     end
   end
 end
