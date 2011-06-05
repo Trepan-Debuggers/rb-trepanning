c0d765e8672211f5aae0e77c1e0773b42fed45e6
rocky <rockyb@rubyforge.org>
2011-06-05 16:29:50 -0400

    Add an "edit" command unit test.

---
+++ test/unit/test-cmd-edit.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+require_relative './mock-helper'
+require_relative './cmd-helper'
+require_relative '../../processor/command/edit'
+
+class TestCommandEdit < Test::Unit::TestCase
+  include MockUnitHelper
+  def setup
+    @name = File.basename(__FILE__, '.rb').split(/-/)[2]
+    $msgs = []
+    $errmsgs = []
+    common_setup(@name)
+    def @cmd.msg(message)
+      $msgs << message
+    end
+    def @cmd.errmsg(message)
+      $errmsgs << message
+    end
+  end
+
+  def test_basic
+    old_editor = ENV['EDITOR']
+    ENV['EDITOR'] = '#'
+    base_file = File.basename(__FILE__)
+    @cmd.proc.settings[:basename] = true
+    @cmd.run([@name])
+    assert_equal "Running # +39 \"edit.rb\"...", $msgs[-1]
+    @cmd.run([@name, 7])
+    assert_equal "Running # +7 \"edit.rb\"...", $msgs[-1]
+    @cmd.run([@name, __FILE__])
+    assert_equal "Running # +1 \"#{base_file}\"...", $msgs[-1]
+  end
+end
