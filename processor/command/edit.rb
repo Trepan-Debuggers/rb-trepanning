61421e12d014df8cd8c1f7c3093b9d061b468d85
rocky <rockyb@rubyforge.org>
2011-06-04 21:51:42 -0400

    Work on edit command via rb8-trepanning.

--- processor/command/edit.rb
+++ processor/command/edit.rb
@@ -8,38 +8,57 @@ class Trepan::Command::EditCommand < Trepan::Command
   $VERBOSE    = nil
   NAME        = File.basename(__FILE__, '.rb')
   HELP    = <<-HELP
-#{NAME} [LOCATION]
+#{NAME} [[FILE] [LINE]]
 
 With no argument, edits file containing most recent line listed.
+The value of the environment variable EDITOR is used for the
+editor to run. If no EDITOR environment variable is set /bin/ex
+is used. The editor should support line and file positioning via
+   editor-name +line file-name
+(Most editors do.)
 
-Editing targets can also be specified.
+Examples:
+#{NAME}            # Edit current location
+#{NAME} 7          # Edit current file at line 7
+#{NAME} test.rb    # Edit test.rb, line 1
+#{NAME} test.rb 10 # Edit test.rb  line 10
       HELP
 
   ALIASES       = %w(e)
   CATEGORY      = 'files'
   NEED_STACK    = false
-  SHORT_HELP    = 'Edit specified file or function'
+  SHORT_HELP    = 'Invoke an editor on some source code'
+  MAX_ARGS      = 2
   $VERBOSE      = old_verbose 
 
+  # FIXME: redo with locations and kparse.
   def run(args)
-    if args.size == 1
-
-      unless true # @state.context
-        errmsg "We are not in a state that has an associated file.\n"
-        return 
+    case args.size
+    when 1
+      file = @proc.frame.source_container[1]
+      line = @proc.frame.source_location[0]
+    when 2
+      line = Integer(args[1]) rescue nil
+      if line 
+        file = @proc.frame.source_container[1]
+      else 
+        file = args[1]
+        line = 1
       end
+    when 3
+      line, file =  args[2], args[1]
     else
-      text = @proc.cmd_argstr
+      errmsg "edit needs at most 2 args." 
     end
     editor = ENV['EDITOR'] || '/bin/ex'
-    file = @proc.frame.file
     if File.readable?(file)
-      edit_cmd = "#{editor} +#{@proc.frame.line} \"#{file}\""
+      file = File.basename(file) if settings[:basename]
+      edit_cmd = "#{editor} +#{line} \"#{file}\""
       msg "Running #{edit_cmd}..."
       system(edit_cmd)
       msg "Warning: return code was #{$?.exitstatus}" if $?.exitstatus != 0
     else
-      errmsg "File \"#{file}\" is not readable.\n"
+      errmsg "File \"#{file}\" is not readable."
     end
   end
 end
@@ -47,6 +66,8 @@ end
 if __FILE__ == $0
   require_relative '../mock'
   dbgr, cmd = MockDebugger::setup
-  dbgr, cmd = MockDebugger::setup
-  cmd.run [cmd.name] if ARGV.size > 0
+  ENV['EDITOR'] = 'echo FAKE-EDITOR'
+  cmd.run [cmd.name] 
+  cmd.run [cmd.name, '7'] 
+  cmd.run [cmd.name, __FILE__, '10'] 
 end
