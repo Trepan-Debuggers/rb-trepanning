d178a7bab4bd47d868970a2a41210beae23e17a9
rocky <rockyb@rubyforge.org>
2011-06-08 15:36:38 -0400

    Add rudimentary pp (pretty-print) command.

---
+++ processor/command/info_subcmd/macro.rb
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2010, 2011 Rocky Bernstein <rockyb@rubyforge.net>
+require_relative '../base/subcmd'
+require_relative '../../../app/complete'
+
+class Trepan::Subcommand::InfoMacro < Trepan::Subcommand
+  unless defined?(HELP)
+    Trepanning::Subcommand.set_name_prefix(__FILE__, self)
+    HELP = <<-HELP
+#{CMD} 
+#{CMD} *
+#{CMD} MACRO1 [MACRO2 ..]
+
+In the first form a list of the existing macro names are shown
+in column format.
+
+In the second form, all macro names and their definitions are show.
+
+In the last form the only definitions of the given macro names is shown.
+    HELP
+    SHORT_HELP = "Info defined macros"
+    MIN_ABBREV = 'mac'.size
+  end
+
+  def complete(prefix)
+    Trepan::Complete.complete_token(@proc.macros.keys + %w(*), prefix)
+  end
+
+  def run(args)
+    if args.size > 2
+      macro_names = 
+        if args.size == 3 && '*' == args[2] 
+          @proc.macros.keys
+        else
+          args[2..-1]
+        end        
+      macro_names.each do |macro_name|
+        if @proc.macros.member?(macro_name)
+          section "#{macro_name}:"
+          string = @proc.macros[macro_name][1]
+          msg "  #{@proc.ruby_format(string)}", {:unlimited => true}
+        else
+          errmsg '%s is not a defined macro' % macro_name
+        end
+      end
+    elsif @proc.macros.empty?
+      msg 'No macros defined.'
+    else
+      msg columnize_commands(@proc.macros.keys.sort)
+    end
+  end
+
+end
+
+if __FILE__ == $0
+  # Demo it.
+  $0 = __FILE__ + 'notagain' # So we don't run this again
+  require_relative '../../mock'
+  cmd = MockDebugger::sub_setup(Trepan::Subcommand::InfoMacro)
+  cmd.run(cmd.prefix + %w(u foo))
+end
---
+++ processor/command/pp.rb
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011 Rocky Bernstein <rockyb@rubyforge.net>
+require 'rubygems'
+require 'pp'
+require_relative 'base/cmd'
+class Trepan::Command::PPCommand < Trepan::Command
+
+  unless defined?(HELP)
+    NAME = File.basename(__FILE__, '.rb')
+    HELP = 
+      "#{NAME} EXPRESSION
+
+Prtty Print the value of the EXPRESSION. Variables accessible are
+those of the environment of the selected stack frame, plus globals.
+
+If the length output string large, the first part of the value is
+shown and ... indicates it has been truncated.
+
+See 'set max string' to change the string truncation limit.
+"
+    CATEGORY      = 'data'
+    SHORT_HELP    = 'pretty print expression truncating long output'
+  end
+  
+  def run(args)
+    obj = @proc.debug_eval(@proc.cmd_argstr)
+    msg (obj.respond_to?(:pretty_inspect) ? obj.pretty_inspect : obj.inspect)
+  end
+end
+        
+if __FILE__ == $0
+  require_relative '../mock'
+  dbgr, cmd = MockDebugger::setup
+  ['(0..10).to_a', '$LOADED_FEATURES'].each do |expr_str|
+    cmd_argstr = expr_str
+      cmd.proc.instance_variable_set('@cmd_argstr', cmd_argstr)
+    cmd.run([cmd.name, cmd_argstr])
+    puts '-' * 20
+  end
+end
