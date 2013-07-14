7c46aab7a326edc34d802512f20fc461e5c81e3f
rocky <rockyb@rubyforge.org>
2013-07-14 18:13:30 -0400

    Columnize is changing, so we can use that for testing here.

--- test/unit/test-cmd-parse_list_cmd.rb
+++ test/unit/test-cmd-parse_list_cmd.rb
@@ -20,9 +20,9 @@ class TestCommandParseListCmd < Test::Unit::TestCase
      ["#{__FILE__} 10", [short_file, 5, 14]],
      ['tmpdir.rb', ['tmpdir.rb', 1, listsize]],
      ['tmpdir.rb 10', ['tmpdir.rb', 5, 5+listsize-1]],
-     ['Columnize.columnize 15', ['columnize.rb', 10, 10+listsize -1]],
-     ['Columnize.columnize 30 3', ['columnize.rb', 30, 32]],
-     ['Columnize.columnize 40 50', ['columnize.rb', 40, 50]],
+#     ['Columnize.columnize 15', ['columnize.rb', 10, 10+listsize -1]],
+#     ['Columnize.columnize 30 3', ['columnize.rb', 30, 32]],
+#     ['Columnize.columnize 40 50', ['columnize.rb', 40, 50]],
     ].each do |arg_str, expect|
       got = @cmdproc.parse_list_cmd(arg_str, listsize, listsize/2)[1..-1]
       got[0] = File.basename(got[0])
