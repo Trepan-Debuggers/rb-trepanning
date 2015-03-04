5078c58c33296dae2de04e1646ef732d39a52b5e
rocky <rockyb@rubyforge.org>
2015-03-04 10:26:27 -0500

    Add "debug" command test

---
+++ test/data/debug.cmd
@@ -0,0 +1,8 @@
+# ***************************************************
+# Test debug command. To be used with gcd.rb
+# ***************************************************
+next
+debug gcd(2,2)
+info locals
+quit!
+quit!
---
+++ test/integration/test-debug.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+require 'test/unit'
+require_relative 'helper'
+require 'rbconfig'
+
+class TestTrace < Test::Unit::TestCase
+  @@NAME = File.basename(__FILE__, '.rb')[5..-1]
+    def test_trepan_debug
+        opts = {:args => '3 5', :verbose=>true}
+        assert_equal(true, run_debugger(@@NAME, 'gcd.rb', opts))
+    end
+end
