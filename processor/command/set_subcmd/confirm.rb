d4f53a1a75b02bb36123f5b7755367e66ad90875
rocky <rockyb@rubyforge.org>
2015-03-08 08:55:32 -0400

    More markdown doc

--- processor/command/set_subcmd/confirm.rb
+++ processor/command/set_subcmd/confirm.rb
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2011 Rocky Bernstein <rockyb@rubyforge.net>
+# Copyright (C) 2011, 2015 Rocky Bernstein <rockyb@rubyforge.net>
 require_relative '../base/subcmd'
 
 class Trepan::Subcommand::SetConfirm < Trepan::SetBoolSubcommand
   unless defined?(HELP)
     Trepanning::Subcommand.set_name_prefix(__FILE__, self)
-    HELP = "Set whether to confirm potentially dangerous operations."
+    HELP = <<-EOH
+**#{PREFIX.join(' ')}**
+
+Some commands have drastic effects, so in interactive mode, we confirm
+these operations. Some commands have a ! suffix to disable that
+confirmation for that specific instance. This command enables or
+disables confirmation across *all* potentially dangerous commands,
+
+See also:
+---------
+
+`show confirm`
+EOH
+    SHORT_HELP = "Set whether to confirm potentially dangerous operations."
     IN_LIST    = true
     MIN_ABBREV = 'co'.size
   end
--- processor/command/show_subcmd/highlight.rb
+++ processor/command/show_subcmd/highlight.rb
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2011 Rocky Bernstein <rockyb@rubyforge.net>
+# Copyright (C) 2011, 2015 Rocky Bernstein <rockyb@rubyforge.net>
 require_relative '../base/subsubcmd'
 require_relative '../base/subsubmgr'
 
 class Trepan::Subcommand::ShowHighlight < Trepan::ShowBoolSubcommand
   unless defined?(HELP)
     Trepanning::Subcommand.set_name_prefix(__FILE__, self)
-    HELP         = 'Show whether we use terminal highlighting'
+    HELP = <<-EOH
+**#{PREFIX.join(' ')}**
+
+Show whether we use terminal highlighting.
+
+See also:
+---------
+
+`set highlight`
+EOH
+    SHORT_HELP   = 'Show whether we use terminal highlighting'
     MIN_ABBREV   = 'high'.size
   end
 
   def run(args)
-    val = :term == @proc.settings[:highlight] 
+    val = :term == @proc.settings[:highlight]
     onoff = show_onoff(val)
     msg("%s is %s." % [@name, onoff])
   end
